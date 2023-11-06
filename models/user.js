dc0c98b95555769300cca474c3f30c75ef2f85c8
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-06 19:47:29 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,24 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: [true, "Email already exist!"],
+    required: [true, "Email is required!"],
+  },
+  username: {
+    type: String,
+    required: [true, "UserName is required!"],
+    match: [
+      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-0._]+(?<![_.])$/,
+      "UserName invalid, it should contain 8-20 alphanumeric letters and be unique!",
+    ],
+  },
+  image: {
+    type: String,
+  },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
