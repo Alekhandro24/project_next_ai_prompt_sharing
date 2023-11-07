9a7b1d12840208c80592527661aa6f2e8b2a8d24
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-07 16:37:22 +0200

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,33 +11,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-    session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      //serverless => lambda => dynamobd
-      await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        //serverless => lambda => dynamobd
+        await connectToDB();
 
-      // ckeck if a user already exist
-      const userExist = await User.findOne({ email: profile.email });
-      // if not - create a new user
-      if (!userExist) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // ckeck if a user already exist
+        const userExist = await User.findOne({ email: profile.email });
+        // if not - create a new user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
