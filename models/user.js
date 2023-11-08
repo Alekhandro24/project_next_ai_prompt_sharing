ff3ffeea46197a174ce1327dffe6a5b5954a30d4
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-08 15:42:54 +0200

    update models\user.js

--- models/user.js
+++ models/user.js
@@ -3,15 +3,15 @@ import { Schema, model, models } from "mongoose";
 const UserSchema = new Schema({
   email: {
     type: String,
-    unique: [true, "Email already exist!"],
+    unique: [true, "Email already existS!"],
     required: [true, "Email is required!"],
   },
   username: {
     type: String,
     required: [true, "UserName is required!"],
     match: [
-      /^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-0._]+(?<![_.])$/,
-      "UserName invalid, it should contain 8-20 alphanumeric letters and be unique!",
+      /^(?=.{4,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-0._]+(?<![_.])$/,
+      "UserName invalid, it should contain 4-20 alphanumeric letters and be unique!",
     ],
   },
   image: {
