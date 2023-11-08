fffa29b5855156ce33cce9c7a69049b8c459fed7
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-08 15:41:58 +0200

    update app\profile\page.jsx

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,29 +7,47 @@ import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const { data: sessions } = useSession();
-  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${sessions?.user.id}/posts`);
       const data = await response.json();
-      setPosts(data);
+      setMyPosts(data);
     };
     if (sessions?.user.id) fetchPosts();
-  }, []);
+  }, [sessions?.user.id]);
 
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
-  const handleDelete = async (post) => {};
+
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: "DELETE",
+        });
+
+        const filteredPosts = myPosts.filter((item) => item._id !== post._id);
+        setMyPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   return (
     <div>
       <Profile
         name="My"
-        descr="Welcome to your personalized profile page"
-        data={posts}
+        descr="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+        data={myPosts}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
       />
