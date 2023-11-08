ab22a674ad824c32ba58e9d94b42511f6b233c4d
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-08 15:40:35 +0200

    update app\api\prompt\[id]\route.js

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -16,23 +16,25 @@ export const GET = async (request, { params }) => {
 };
 
 //PATCH(update)
-export const Patch = async (request, { params }) => {
+export const PATCH = async (request, { params }) => {
   const { prompt, tag } = await request.json();
 
   try {
     await connectToDB();
 
+    // Find the existing prompt by ID
     const existingPrompt = await Prompt.findById(params.id);
 
     if (!existingPrompt)
       return new Response("Prompt not found", { status: 404 });
 
+    // Update the prompt with new data
     existingPrompt.prompt = prompt;
     existingPrompt.tag = tag;
 
     await existingPrompt.save();
 
-    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+    return new Response("Successfully updated the Prompts", { status: 200 });
   } catch (error) {
     return new Response("Failed to update prompt", { status: 500 });
   }
@@ -43,10 +45,11 @@ export const DELETE = async (request, { params }) => {
   try {
     await connectToDB();
 
+    // Find the prompt by ID and remove it
     await Prompt.findByIdAndDelete(params.id);
 
     return new Response("Prompt has been deleted successfuly", { status: 200 });
   } catch (error) {
-    return new Response("Failed to delete prompt", { status: 500 });
+    return new Response("Error deleting prompt", { status: 500 });
   }
 };
