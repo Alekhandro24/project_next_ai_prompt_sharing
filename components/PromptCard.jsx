22ffa88d5c6233b81d7d1d036fe3d0509dd58d78
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-11-16 15:10:43 +0200

    Update PromptCard.jsx

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -65,11 +65,7 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         </div>
       </div>
 
-      <p className="my-4 font-satoshi text-sm text-gray-700">
-        {post.prompt.length > 100
-          ? `${post.prompt.substring(0, 100)}...`
-          : post.prompt}
-      </p>
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
       <p
         className="font-inter text-sm blue_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
